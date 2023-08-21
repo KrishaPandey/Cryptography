4b8365b684b511afe6f29e12813f109269cebdb2
KrishaPandey <83869553+KrishaPandey@users.noreply.github.com>
2023-08-21 20:57:23 +0545

    DONE

--- LAB1/railfence.py
+++ LAB1/railfence.py
@@ -3,20 +3,19 @@ def getCipher(text, rails):
     length_row = 0
     extra_char = 0
 
-    # choosing in what interval a text is to be selected to arrange in a row.
+    
     for i in range(rails):
         ctext = ""
         for j in range(i, len(text), rails):
             ctext += text[j]
 
         if i == 0:
-            length_row = len(ctext)  # setting length of first row to add extra alphabets later in other rows to match dimension
-
+            length_row = len(ctext) 
         if i != 0 and len(ctext) < length_row:
             ctext += "X"  # adding extra alphabet
             extra_char += 1
 
-        ciphertext += ctext # concatenating the finalised cipher part of a row into cipertext
+        ciphertext += ctext  
 
         print(ctext)
 
@@ -28,11 +27,11 @@ def getDecipher(text, rails):
     deciphertext = ""
     length = int(len(text) / rails)
 
-    # Outer loop runs through the length of columns
+  
     for i in range(0, length):
         # Inner loop runs though the rows of columns
         for j in range(0, rails):
-            deciphertext += text[length * j + i]  # formula to extract alphabets from correct positions.
+            deciphertext += text[length * j + i]  
 
     return deciphertext
 
